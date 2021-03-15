ecad0744cf9d8fb3c74edec5248dc02920519b5a
omfacu <57021647+omfacu@users.noreply.github.com>
2021-03-15 18:01:03 +0300

    Initial commit

---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+
+    public static Transform[] points;
+
+    void Awake()
+    {
+        points = new Transform[transform.childCount];
+    }
+
+}
